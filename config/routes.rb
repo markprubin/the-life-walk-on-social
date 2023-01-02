08bdaa7f3c4400a8e48bf354482e122ccc8f22de
markprubin <mark.p.rubin@gmail.com>
2023-01-02 13:24:45 -0700

    updates user model, adds index and create function

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+#USERS
+get "/users" => "users#index"
+post "/users" => "users#create"
 
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,6 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
+
+User.create!(first_name: "Mark", last_name: "Rubin", email: "mark@example.com", password: "password", image_url: "https://i.imgur.com/VdJQsjf.png", phone: "555-555-5555", birth_date: "1991-02-22", home_address: "123 Central, Phoenix, AZ 85028", bio: "This is my bio.", admin: true)
+User.create!(first_name: "Tom", last_name: "Hanks", email: "wilson@fedex.com", password: "password", image_url: "https://media1.popsugar-assets.com/files/thumbor/Pm-91vpz-vi3_yrS2PMioQyeGdI/fit-in/1024x1024/filters:format_auto-!!-:strip_icc-!!-/2020/12/16/927/n/1922283/0196b2b55fda78d927ce09.12541490_/i/polar-express-movie-tom-hanks-characters.jpg", phone: "123-555-5555", birth_date: "1956-07-09", home_address: "456 Main St, New York City, NY 10001", bio: "This is Wilson's bio.", admin: false)
