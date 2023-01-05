53b41ccf6c95c375216cd583e86daf4e9d256050
markprubin <mark.p.rubin@gmail.com>
2023-01-05 11:37:11 -0700

    updates

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,5 @@
 class EventsController < ApplicationController
-  before_action :authenticate_admin, except: [:index, :show]
+  # before_action :authenticate_admin, except: [:index, :show]
 
 
   def index
