33725eea54096bc4969774d66f9e7f4ec99be4dc
markprubin <mark.p.rubin@gmail.com>
2023-03-05 14:04:25 -0700

    updates users index.json.jb and user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,8 +7,9 @@ has_secure_password
 validates :email, presence: true, uniqueness: true
 
 
-# def favorite_events
-#   events
-# end
+def favorite_events
+  # events.select{|favorite| favorite.user_id == @user.id }
+  Favorite.where(user_id: id)
+end
 
 end
