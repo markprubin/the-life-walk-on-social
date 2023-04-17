26f80378ed12e0048b233d2dd076c9838f272579
markprubin <mark.p.rubin@gmail.com>
2023-04-17 14:45:48 -0700

    removes unnecessary model code

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -2,14 +2,6 @@ class Favorite < ApplicationRecord
 belongs_to :user
 belongs_to :event
 
-# Map through fav ids, link event id to events table, call event name
 
-  # def event
-    
-  #   fav = Favorite.find_by(id: 45)
-  #   eventid = fav.event_id
-  #   Event.find_by(id: eventid).name
-    
-  # end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,6 @@ validates :email, presence: true, uniqueness: true
 
 
 def favorite_events
-  # events.select{|favorite| favorite.user_id == @user.id }
   Favorite.where(user_id: id)
 end
 
